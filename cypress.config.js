151a2d1e43ccb4f5ff03e68bfc645b8b0bc234d8
Podles <kuba.podlesny@gmail.com>
2023-04-01 14:03:00 +0200

    remove setupNodeEvents to satisfy linter

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
     baseUrl: 'https://www.kiwi.com',
     watchForFileChanges: true,
     env: {
