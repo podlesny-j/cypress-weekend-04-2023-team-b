785256583fbc662e05da2eec428bc918b6b60f16
Eduard Šimek <eduard.simek97@gmail.com>
2023-04-01 11:40:34 +0200

    Moved

--- cypress/e2e/Task1.js
+++ cypress/e2e/Task1.js
@@ -1,16 +1,16 @@
-import 'cypress-plugin-xhr-toggle'
+//import 'cypress-plugin-xhr-toggle'
 
 describe("Add multpile items to basket", () => {
     
 
-   /* before(function() {
+    before(function() {
         
     })
 
     beforeEach(function () {
         
     })
-    */
+    
 
     it("Add specific items to basket", () => {
         cy.log("hello")
