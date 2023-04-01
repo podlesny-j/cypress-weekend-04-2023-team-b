2bd9dbcb273a6d4dd64e7b5c283a1704c2dfec5e
doliakovak <katarina.doliakova@finportal.sk>
2023-04-01 12:55:58 +0200

    chore:overenie meta desc

--- cypress/e2e/task1_katka/task1.cy.js
+++ cypress/e2e/task1_katka/task1.cy.js
@@ -9,7 +9,7 @@ it('Overenie headera', () =>{
     cy.title().should('eq', 'Cheap flights to China | Kiwi.com')
 
     cy.log('overenie meta description')
-
+    cy.get('head meta[name=description]').should('have.attr','content').and('eq','Find the cheapest flights to China. Compare different airlines, choose the best price, and book your cheap plane ticket to China.')
     
 
 })
