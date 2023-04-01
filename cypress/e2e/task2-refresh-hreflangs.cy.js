49bb1cbb2277f183e7db1d39d6b0c0b753df06e8
Podles <kuba.podlesny@gmail.com>
2023-04-01 18:07:11 +0200

    task2 bonus part almost done

--- cypress/e2e/task2-refresh-hreflangs.cy.js
+++ cypress/e2e/task2-refresh-hreflangs.cy.js
@@ -1,6 +1,6 @@
-describe('Refresh hreflangs', () => {
+describe.skip('Refresh hreflangs', () => {
   // this is supposed to be used lcoally to refresh the fixture file and not having to do it manually
-  it('Refresh hreflangs', () => {
+  it('Refresh hreflangs basic', () => {
     let langValue
     let hrefLangsArray = []
 
@@ -15,11 +15,37 @@ describe('Refresh hreflangs', () => {
           langValue = this.attributes.hreflang.value
 
           hrefLangsArray.push(langValue)
-          console.log(langValue)
-          console.log(hrefLangsArray)
         })
         cy.wrap(hrefLangsArray).as('hrefLangsArray')
         cy.writeFile('cypress/fixtures/hrefLangsFlightsLondonIstanbul.json', hrefLangsArray)
+      })
+  })
+
+  it('Refresh hreflangs advanced', () => {
+    let langValue
+    let hrefValue
+    let fixtureLine
+    let fixtureLineArray = []
+
+    cy.visit('/es/cheap-flights/london-united-kingdom/istanbul-turkey/')
+
+    cy.step('Get all <link> elements in <head> with "hreflang" attribute')
+    cy.get('head link')
+      .filter('[hreflang]')
+      .as('hrefLangs')
+      .then($hrefLangs => {
+        $hrefLangs.each(function () {
+          langValue = this.attributes.hreflang.value
+          hrefValue = this.attributes.href.value
+
+          fixtureLine = JSON.stringify(`${hrefValue} : ${langValue}`)
+
+          fixtureLineArray.push(fixtureLine)
+        })
+        cy.writeFile(
+          'cypress/fixtures/hrefLangsFlightsLondonIstanbulAdvanced.json',
+          fixtureLineArray,
+        )
       })
   })
 })
