ac2bb28f0bcf3ea098a7f05277b0069f4f3b5a6b
doliakovak <katarina.doliakova@finportal.sk>
2023-04-01 14:10:19 +0200

    chore: overenie canonical

--- cypress/e2e/task1_katka/task1.cy.js
+++ cypress/e2e/task1_katka/task1.cy.js
@@ -1,18 +1,23 @@
-describe('Overenie stranky', ()=> {
-    before('',()=>{
-        cy.visit('/en/country/china/?botview=1')
-    } )
+describe('Overenie stranky', () => {
+  before('', () => {
+    cy.visit('/en/country/china/?botview=1')
+  })
 
-it('Overenie headera', () =>{
-
+  it('Overenie headera', () => {
     cy.log('overenie title')
     cy.title().should('eq', 'Cheap flights to China | Kiwi.com')
 
     cy.log('overenie meta description')
-    cy.get('head meta[name=description]').should('have.attr','content').and('eq','Find the cheapest flights to China. Compare different airlines, choose the best price, and book your cheap plane ticket to China.')
-    
+    cy.get('head meta[name=description]')
+      .should('have.attr', 'content')
+      .and(
+        'eq',
+        'Find the cheapest flights to China. Compare different airlines, choose the best price, and book your cheap plane ticket to China.',
+      )
+
     cy.log('overenie canonical')
     cy.get('head link[rel=canonical]')
-
-})
+      .should('have.attr', 'href')
+      .and('eq', 'https://www.kiwi.com/en/country/china/')
+  })
 })
