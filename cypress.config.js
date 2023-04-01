ff5016a5d022b44e64e054b329223da661fbae1f
Podles <kuba.podlesny@gmail.com>
2023-04-01 13:27:22 +0200

    watchForFileChanges: no idea why default beheaviour does not work but this is handy

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'https://www.kiwi.com',
+    watchForFileChanges: true,
     env: {
       hideXhr: true,
     },
