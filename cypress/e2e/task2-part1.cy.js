15d2f086fa2b556f5d92786082c1ddcc04a60aac
Podles <kuba.podlesny@gmail.com>
2023-04-01 13:27:53 +0200

    first working test for changed language

--- cypress/e2e/task2-part1.cy.js
+++ cypress/e2e/task2-part1.cy.js
@@ -1,9 +1,34 @@
 describe('task 2 part 1', () => {
-  beforeEach(() => {
-    cy.visit('/mx/cheap-flights/london-united-kingdom/istanbul-turkey/')
-  })
+  // beforeEach(() => {
+  // })
 
   it('check the language is not supported on the visited site', () => {
-    cy.intercept('GET', 'https://cdn9.forter.com/vchk2').as('redirect')
+    const originalTargetPath = '/mx/cheap-flights/london-united-kingdom/istanbul-turkey/'
+    const regex = /\/([a-z]{2})\//
+
+    const requestedLanguage = originalTargetPath.match(regex)
+    expect(
+      requestedLanguage,
+      'Check there is one captured group returned from matched substring',
+    ).to.have.length(2)
+
+    cy.request({
+      url: originalTargetPath,
+      followRedirect: false,
+    }).then(({ headers }) => {
+      const resolvedPath = headers.location
+
+      expect(resolvedPath).to.not.eq(originalTargetPath)
+
+      const resolvedLanguage = resolvedPath.match(regex)
+      expect(
+        resolvedLanguage,
+        'Check there is one captured group returned from matched substring',
+      ).to.have.length(2)
+      expect(resolvedLanguage[1]).not.eq(requestedLanguage[1])
+
+      cy.log(requestedLanguage)
+      cy.log(resolvedLanguage)
+    })
   })
 })
