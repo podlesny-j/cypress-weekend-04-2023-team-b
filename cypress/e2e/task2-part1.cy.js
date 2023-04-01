44c3da6788c9024e19a11eece4f923c18e4c888d
Podles <kuba.podlesny@gmail.com>
2023-04-01 14:34:57 +0200

    working solution for second it block. needs polishing

--- cypress/e2e/task2-part1.cy.js
+++ cypress/e2e/task2-part1.cy.js
@@ -9,26 +9,48 @@ describe('task 2 part 1', () => {
     const requestedLanguage = originalTargetPath.match(regex)
     expect(
       requestedLanguage,
-      'Check there is one captured group returned from matched substring',
+      'Check there is one captured group returned by matched substring',
     ).to.have.length(2)
 
     cy.request({
       url: originalTargetPath,
       followRedirect: false,
-    }).then(({ headers }) => {
+    }).then(({ status, headers }) => {
       const resolvedPath = headers.location
-
-      expect(resolvedPath).to.not.eq(originalTargetPath)
-
       const resolvedLanguage = resolvedPath.match(regex)
+
+      expect(status).to.eq(301)
+      expect(resolvedPath).to.not.eq(originalTargetPath)
       expect(
         resolvedLanguage,
-        'Check there is one captured group returned from matched substring',
+        'Check there is one captured group returned by matched substring',
       ).to.have.length(2)
-      expect(resolvedLanguage[1]).not.eq(requestedLanguage[1])
-
-      cy.log(requestedLanguage)
-      cy.log(resolvedLanguage)
+      expect(resolvedLanguage[1]).to.not.eq(requestedLanguage[1])
     })
+  })
+
+  it.only('try to visit without redirect', () => {
+    let langValue
+    let hrefLangsArray = []
+
+    cy.visit('/mx/cheap-flights/london-united-kingdom/istanbul-turkey/')
+    cy.get('head link')
+      .filter('[hreflang]')
+      .as('hrefLangs')
+      .then($hrefLangs => {
+        $hrefLangs.each(function (index, element) {
+          langValue = element.attributes.hreflang.value
+
+          hrefLangsArray.push(langValue)
+          console.log(langValue)
+          console.log(hrefLangsArray)
+        })
+
+        return hrefLangsArray
+      })
+      .then(hrefLangsArray => {
+        cy.wrap(hrefLangsArray).as('hrefLangsArray')
+        cy.log(hrefLangsArray)
+      })
   })
 })
