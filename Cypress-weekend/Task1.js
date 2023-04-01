aba3fba45b73459b1df2cb5a3f4f1bf5dc836a9a
Eduard Šimek <eduard.simek97@gmail.com>
2023-04-01 11:20:18 +0200

    Init

---
+++ Cypress-weekend/Task1.js
@@ -0,0 +1,23 @@
+import 'cypress-plugin-xhr-toggle'
+
+describe("Add multpile items to basket", () => {
+    
+
+   /* before(function() {
+        
+    })
+
+    beforeEach(function () {
+        
+    })
+    */
+
+    it("Add specific items to basket", () => {
+        cy.log("hello")
+    });
+
+
+
+
+
+});
